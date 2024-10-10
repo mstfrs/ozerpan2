a20517a3f8aa1fab19ddc66c1a3db02562368ac6
mustafaors2024 <mustafa.ors@mail.juniuserp.com>
2024-10-10 11:49:37 +0300

    first commit

---
+++ apps/ozerpan/frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import { FrappeProvider } from 'frappe-react-sdk'
+
+import { Outlet } from 'react-router-dom'
+
+
+function App() {
+
+
+	return (
+		<div className="App">
+			<FrappeProvider
+				siteName={import.meta.env.VITE_SITE_NAME}
+				socketPort={import.meta.env.VITE_SOCKET_PORT}
+				url='http://localhost:8001'
+				tokenParams={{
+					type: 'token',
+					useToken: true,
+					//token:()=>'api_key:api_secret'
+				}}
+			>
+
+				<Outlet />
+			</FrappeProvider>
+		</div>
+	)
+}
+
+
+export default App
